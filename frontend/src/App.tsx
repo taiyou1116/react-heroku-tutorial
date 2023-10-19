265fd05d434991d330d2c7e7bfcfb5bd94345c3f
taiyou <hiromacha1116@icloud.com>
2023-10-20 02:11:42 +0900

    ローカル環境でstoreの表示成功

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-function App() {
+type Fruit = {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+};
+
+export default function App() {
+  const [fruits, setFruits] = React.useState<Fruit[] | null>(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // const res = await fetch("http://localhost:8000/api");
+        const res = await fetch("/api");
+        const json: React.SetStateAction<Fruit[] | null> = await res.json();
+        setFruits(json);
+      } catch (e: unknown) {
+        // if (e instance of Error) {
+        //   console.error(e.message);
+        // }
+        console.error("Err");
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container fruitsList">
+      <h1>Fruits Store</h1>
+
+      {fruits?.map((fruit) => (
+        <div key={fruit.id}>
+          <figure>
+            <img src={fruit.image}alt={fruit.name}/>
+          </figure>
+          <div className="text">
+            <h2>{fruit.name}</h2>
+            <p>{`￥${fruit.price}`}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
-
-export default App;
+};
